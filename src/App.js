3b609be057e93ad091f3f88c357850321c248f46
sunaycansev <fsunaycansev@gmail.com>
2021-10-29 20:01:59 +0300

    init redux and add getNotesAsync

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useSelector } from "react-redux";
+import NoteInput from "./components/NoteInput";
+import NoteList from "./components/NoteList";
+
 function App() {
+  const items = useSelector((state) => state.notes.items);
   return (
-    <div className="justify-content-center d-flex mt-3">
-      <h1>Note Taking App</h1>
-    </div>
+    <>
+      <div className="justify-content-center d-flex mt-3">
+        <h1>Note Taking App</h1>
+      </div>
+      <div>
+        <NoteInput />
+      </div>
+      <div>
+        <NoteList />
+      </div>
+    </>
   );
 }
 
